f699950ee80c20cef9a9363f5fe38491fb356b87
OvejaBasualdo <elportenio@gmail.com>
2023-01-23 17:16:31 -0300

    Adding DTO

---
+++ src/main/java/com/accenture/paymentModule/dtos/UserDTO.java
@@ -0,0 +1,14 @@
+package com.accenture.paymentModule.dtos;
+
+import java.util.Set;
+
+public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String lastName;
+    private String dni;
+    private String email;
+    private String password;
+    private Set<AccountDTO> accounts;
+}
