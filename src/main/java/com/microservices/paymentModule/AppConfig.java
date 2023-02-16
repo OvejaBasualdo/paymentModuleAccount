70571ec9f34be75fa62f983917dad09f3efa0707
OvejaBasualdo <elportenio@gmail.com>
2023-02-16 14:28:50 -0300

    Another modifications

--- src/main/java/com/microservices/paymentModule/AppConfig.java
+++ src/main/java/com/microservices/paymentModule/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
-    @Bean("userRest")
+    @Bean("accountRest")
     public RestTemplate registryAccountRestTemplate(){
         return new RestTemplate();
     }
--- src/main/java/com/microservices/paymentModule/dtos/TransactionDTO.java
+++ src/main/java/com/microservices/paymentModule/dtos/TransactionDTO.java
@@ -3,21 +3,53 @@ package com.microservices.paymentModule.dtos;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class TransactionDTO {
 
+    private Long id;
     private BigDecimal amount;
     private String fromAccount;
     private String toAccount;
-    private String transactionType;
+    private String paymentType;
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate scheduledDate;
 
-    public String getTransactionType() {
-        return transactionType;
+    public Long getId() {
+        return id;
     }
 
-    public void setTransactionType(String transactionType) {
-        this.transactionType = transactionType;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "TransactionDTO{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", fromAccount='" + fromAccount + '\'' +
+                ", toAccount='" + toAccount + '\'' +
+                ", paymentType='" + paymentType + '\'' +
+                ", scheduledDate=" + scheduledDate +
+                '}';
+    }
+
+    public LocalDate getScheduledDate() {
+        return scheduledDate;
+    }
+
+    public void setScheduledDate(LocalDate scheduledDate) {
+        this.scheduledDate = scheduledDate;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
     }
 
     public BigDecimal getAmount() {
